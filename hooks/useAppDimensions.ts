7905014730de98ad917d6068f880d6d9c0304118
Alexandre Moreira <brainmunchers@gmail.com>
2023-11-30 16:12:26 +0100

    stricter mobile detection

--- hooks/useAppDimensions.ts
+++ hooks/useAppDimensions.ts
@@ -9,7 +9,7 @@ export default function useAppDimensions() {
   const { width } = useWindowSize();
 
   useEffect(() => {
-    if (DeviceDetect.isMobile) {
+    if (DeviceDetect.isMobileOnly) {
       setIsMobile(true);
     }
   }, []);
