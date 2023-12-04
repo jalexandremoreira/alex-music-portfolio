39f9beed7cb09d0d525942987ccb5d8e9f984d0b
Alexandre Moreira <brainmunchers@gmail.com>
2023-12-04 11:49:30 +0100

    add mobile layout to contact page

--- app/contact-me/page.tsx
+++ app/contact-me/page.tsx
@@ -13,96 +13,186 @@ import useAppDimensions from '@/hooks/useAppDimensions';
 import { Email, Phone } from '@/components/Icons';
 
 export default function ContactMePage() {
-  const { maxWidthDesktop, paddingXDesktop } = useAppDimensions();
+  const {
+    isMobile,
+    maxWidthDesktop,
+    paddingTopDesktop,
+    paddingTopMobile,
+    paddingXDesktop,
+    paddingXMobile,
+  } = useAppDimensions();
 
   const { palette } = theme;
+
+  const info = {
+    email: 'alexmoreiramusic@gmail.com',
+    mobile: '+46 (0)76 837 56 62',
+    moreInfo: `For information regarding services, fees, availability or information regarding the studio, please contact me`,
+    link: 'linktr.ee/jalexandremoreira',
+  };
+
+  if (!isMobile)
+    return (
+      <Stack
+        alignItems="center"
+        height="100%"
+        justifyContent="center"
+        width="100%"
+      >
+        <Stack
+          alignItems="center"
+          direction="row"
+          gap="50px"
+          justifyContent="center"
+          marginBottom="60px"
+          maxWidth={maxWidthDesktop}
+          paddingTop={paddingTopDesktop}
+          paddingX={paddingXDesktop}
+          width="100%"
+        >
+          <Stack maxWidth="40%" gap="40px">
+            <Header
+              className="titles prevent-select"
+              color="white.main"
+              text="let's chat!"
+              variant="h3"
+            />
+
+            <Typography
+              color="white.main"
+              variant="h5"
+              className="prevent-select"
+            >
+              {info.moreInfo}
+            </Typography>
+
+            <Stack gap="20px">
+              <Accordion
+                icon={<Phone size={28} color={palette.primary[600]} />}
+                title="phone number"
+              >
+                <Typography color="white.main" variant="h5">
+                  {info.mobile}
+                </Typography>
+              </Accordion>
+
+              <Accordion
+                icon={<Email size={28} color={palette.primary[600]} />}
+                title="email"
+              >
+                <Typography color="white.main" variant="h5">
+                  {info.email}
+                </Typography>
+              </Accordion>
+            </Stack>
+
+            <Links
+              color={palette.primary[600]}
+              hoverColor={palette.primary[700]}
+              spaceBetween
+            />
+          </Stack>
+
+          <Box bgcolor="primary.600" width="2px" height="100%" />
+
+          <Stack gap="20px">
+            <QR
+              bgColor={palette.primary[600]}
+              color={palette.primary.main}
+              size={350}
+            />
+
+            <Link href={`https://${info.link}`} target="_blank">
+              <Typography
+                textAlign="center"
+                color="primary.600"
+                fontWeight="bold"
+                sx={{
+                  transition: 'color 0.1s ease',
+                  '&:hover': { color: 'primary.700' },
+                }}
+              >
+                {info.link}
+              </Typography>
+            </Link>
+          </Stack>
+        </Stack>
+      </Stack>
+    );
 
   return (
     <Stack
       alignItems="center"
-      height="100%"
+      gap="20px"
       justifyContent="center"
+      paddingTop={paddingTopMobile}
+      paddingX={paddingXMobile}
       width="100%"
     >
-      <Stack
-        alignItems="center"
-        direction="row"
-        gap="50px"
-        justifyContent="center"
-        marginBottom="60px"
-        maxWidth={maxWidthDesktop}
-        paddingTop="100px"
-        paddingX={paddingXDesktop}
-        width="100%"
-      >
-        <Stack maxWidth="40%" gap="40px">
-          <Header
-            className="titles prevent-select"
-            color="white.main"
-            text="let's chat!"
-            variant="h3"
-          />
+      <QR
+        bgColor={palette.primary[600]}
+        color={palette.primary.main}
+        size={350}
+      />
 
-          <Typography
-            color="white.main"
-            variant="h5"
-            className="prevent-select"
-          >
-            For information regarding services, fees, availability or
-            information regarding the studio, please contact me
-          </Typography>
+      <Link href={`https://${info.link}`} target="_blank">
+        <Typography
+          textAlign="center"
+          color="primary.600"
+          fontWeight="bold"
+          sx={{
+            transition: 'color 0.1s ease',
+            '&:hover': { color: 'primary.700' },
+          }}
+        >
+          {info.link}
+        </Typography>
+      </Link>
 
-          <Stack gap="20px">
-            <Accordion
-              icon={<Phone size={28} color={palette.primary[600]} />}
-              title="phone number"
-            >
-              <Typography color="white.main" variant="h5">
-                +46 (0)76 837 56 62
-              </Typography>
-            </Accordion>
+      <Box bgcolor="primary.600" height="2px" width="100%" />
 
-            <Accordion
-              icon={<Email size={28} color={palette.primary[600]} />}
-              title="email"
-            >
-              <Typography color="white.main" variant="h5">
-                alexmoreiramusic@gmail.com
-              </Typography>
-            </Accordion>
-          </Stack>
-
-          <Links
-            color={palette.primary[600]}
-            hoverColor={palette.primary[700]}
-            spaceBetween
-          />
-        </Stack>
-
-        <Box bgcolor="primary.600" width="2px" height="100%" />
-
-        <Stack gap="20px">
-          <QR
-            bgColor={palette.primary[600]}
-            color={palette.primary.main}
-            size={350}
-          />
-
-          <Link href="https://linktr.ee/jalexandremoreira" target="_blank">
-            <Typography
-              textAlign="center"
-              color="primary.600"
-              fontWeight="bold"
-              sx={{
-                transition: 'color 0.1s ease',
-                '&:hover': { color: 'primary.700' },
-              }}
-            >
-              linktr.ee/jalexandremoreira
-            </Typography>
-          </Link>
-        </Stack>
+      <Stack alignItems="center">
+        <Header
+          className="titles prevent-select"
+          color="white.main"
+          text="let's chat!"
+          variant="h4"
+        />
       </Stack>
+
+      <Typography color="white.main" className="prevent-select">
+        {info.moreInfo}
+      </Typography>
+
+      <Stack gap="20px" width="100%">
+        <Accordion
+          icon={<Phone size={20} color={palette.primary[600]} />}
+          title="phone number"
+        >
+          <Typography color="white.main" variant="h5">
+            {info.mobile}
+          </Typography>
+        </Accordion>
+
+        <Accordion
+          icon={<Email size={20} color={palette.primary[600]} />}
+          title="email"
+        >
+          <Typography color="white.main" variant="h5">
+            {info.email}
+          </Typography>
+        </Accordion>
+      </Stack>
+
+      <Stack width="100%">
+        <Links
+          color={palette.primary[600]}
+          hoverColor={palette.primary[700]}
+          spaceBetween
+        />
+      </Stack>
+
+      <Box />
     </Stack>
   );
 }
